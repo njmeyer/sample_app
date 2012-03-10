43f3cde4afd5d5ca397c2edfffc21930f9aeb4d5
Nathan Meyer <njmeyer@gmail.com>
2012-03-10 16:54:34 -0500

    Fixed routes.db for Home page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ SampleApp::Application.routes.draw do
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
   
-  root :to => "welcome#index"
+  root :to => "pages#home"
 #  get "pages/home"
 #  get "pages/contact"
 #  get "pages/about" 
